ad2c32a792e911abdc48ad2746727f18d5851571
VatsalNaynak <42770355+VatsalNaynak@users.noreply.github.com>
2019-10-10 13:33:45 +0530

    Updates

---
+++ precache-manifest.da634f14228fe64416608e27eecfbc0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07cc767cb789bc85b30becacb72bc60f",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "36ee431f9be889bcc447",
+    "url": "/ecommerce/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "b1fb82693205fd6024bd",
+    "url": "/ecommerce/static/css/main.5053a854.chunk.css"
+  },
+  {
+    "revision": "36ee431f9be889bcc447",
+    "url": "/ecommerce/static/js/2.421bbf7e.chunk.js"
+  },
+  {
+    "revision": "b1fb82693205fd6024bd",
+    "url": "/ecommerce/static/js/main.2a91ad5b.chunk.js"
+  },
+  {
+    "revision": "9aa1de8f5f4220762f76",
+    "url": "/ecommerce/static/js/runtime-main.234f0e98.js"
+  }
+]);

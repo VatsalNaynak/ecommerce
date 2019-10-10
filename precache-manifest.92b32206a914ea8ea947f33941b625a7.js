65f641cbfa18cdc960815606b5a55307c755b9d1
VatsalNaynak <42770355+VatsalNaynak@users.noreply.github.com>
2019-10-10 13:49:17 +0530

    Updates

--- precache-manifest.92b32206a914ea8ea947f33941b625a7.js
+++ precache-manifest.92b32206a914ea8ea947f33941b625a7.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "07cc767cb789bc85b30becacb72bc60f",
+    "revision": "8fe3df65cd92892e1a18bc99b8ba3f86",
     "url": "/ecommerce/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ecommerce/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "b1fb82693205fd6024bd",
+    "revision": "7d192e1f7d5fc6c70c1e",
     "url": "/ecommerce/static/css/main.5053a854.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ecommerce/static/js/2.421bbf7e.chunk.js"
   },
   {
-    "revision": "b1fb82693205fd6024bd",
-    "url": "/ecommerce/static/js/main.2a91ad5b.chunk.js"
+    "revision": "7d192e1f7d5fc6c70c1e",
+    "url": "/ecommerce/static/js/main.8750ed07.chunk.js"
   },
   {
     "revision": "9aa1de8f5f4220762f76",
